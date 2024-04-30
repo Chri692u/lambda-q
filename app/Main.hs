9c620939bfa1618fa92e2daad4b5b1dc5b1cdaab
Christoffer Lind Andersen <57995582+Chri692u@users.noreply.github.com>
2024-04-30 15:48:16 +0200

    test typechecking, parser is still bad :dog:

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import qualified Runtime.Filesystem as FS
 import Language.Syntax
 import Language.Types
 import Language.Parser
+import Language.Check
 import Language.Eval
 import System.Directory
 import System.FilePath
@@ -35,14 +36,20 @@ main = do
   case st of
     Left err -> putStrLn err
     Right state -> do
-      let code1 = pack "(\\x : string . `touch x`) \"test.txt\""
+      let id = pack "(\\x : string . x) \"test.txt\""
+      let touch = pack "(\\x : string . touch x)"
       let code2 = pack "`\"haha\"` , \"test\" , \"haha\""
       let code3 = pack "(touch \"1.txt\") ; (touch \"2.txt\")"
       let code4 = pack "_(\\x : q string . x) `\"haha\"`_"
-      let res = parseLine code3
+      let res = parseLine touch
       case res of
         Left err -> putStrLn $ errorBundlePretty err
         Right exp -> do
           putStrLn $ "Parsed: " ++ show exp
-          (val, s) <- eval exp state
-          print val
+          let checked = typecheck E.empty exp
+          case checked of
+            Left err -> putStrLn err
+            Right t -> do
+              putStrLn $ "Typechecked: " ++ show t
+              (val, st) <- eval exp state
+              putStrLn $ "Evaluated: " ++ show val
